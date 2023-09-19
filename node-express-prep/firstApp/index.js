b4c03a0b36b699e51ec3982527eb050ecad7fce5
itsoknottobeok <36960178+itsoknottobeok@users.noreply.github.com>
2023-09-18 20:49:49 -0700

    Set basic GET and POST request

--- node-express-prep/firstApp/index.js
+++ node-express-prep/firstApp/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    res.send('<h1>This is the webpage you requested</h>');
+// this matches every single request
+// app.use((req, res) => {
+//     res.send('<h1>This is the webpage you requested</h>');
+// })
+
+app.get('/', (req, res) => {
+    res.send('<h1>This is a home page</h>');
+})
+
+app.get('/cats', (req, res) => {
+    res.send('<h1>Meow</h>');
+})
+
+app.post('/cats', (req, res) => {
+    res.send('<h1>This is a POST request for /cats</h>')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('<h1>Woof</h>');
+})
+
+// match every GET request
+app.get('*', (req, res) => {
+    res.send('<h1>The path doesn\'t exist...</h>');
 })
 
 app.listen(3000, () => {
