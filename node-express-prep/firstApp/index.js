6ce0ceee8358efb4d71341d6453a76704ee5b57a
itsoknottobeok <36960178+itsoknottobeok@users.noreply.github.com>
2023-09-18 21:39:27 -0700

    Work with query string

--- node-express-prep/firstApp/index.js
+++ node-express-prep/firstApp/index.js
@@ -19,6 +19,7 @@ app.get('/r/:subreddit/:postId', (req, res) => {
     const { subreddit, postId } = req.params;
     res.send(`<h1>Viewing post ID: ${postId} on the ${subreddit} subreddit`);
 })
+
 app.get('/cats', (req, res) => {
     res.send('<h1>Meow</h>');
 })
@@ -29,6 +30,17 @@ app.post('/cats', (req, res) => {
 
 app.get('/dogs', (req, res) => {
     res.send('<h1>Woof</h>');
+})
+
+// query strings: after ? in url
+// http://localhost:3000/search/?q=happy
+app.get('/search', (req, res) => {
+    const { q } = req.query;
+    if (!q) {
+        res.send(`<h1>Nothing found if nothing searched</h>`);
+        // end after res.send
+    }
+    res.send(`<h1>Search results for: ${q}</h>`);
 })
 
 // match every GET request
